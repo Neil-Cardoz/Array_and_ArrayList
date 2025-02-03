5fd6888fd65f1d7cc25a16cbc3629f1654fa1e48
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-02-03 21:23:02 +0530

    Separate odd and even numbers into lists
    Implemented functionality to separate odd and even numbers from an input array into two different ArrayLists, odd and eve. The array is iterated through, and each element is checked using the modulus operation to determine if it is even or odd. Even numbers are added to the eve list, and odd numbers are added to the odd list. Finally, the lists are printed using the display method.

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -6,7 +6,28 @@ import java.util.Arrays;
 class ArrayFunctions {
     public void oddEven(int[] arr)
     {
-        
+        ArrayList<Integer> odd = new ArrayList<Integer>();
+        ArrayList<Integer> eve = new ArrayList<Integer>();
+        int odd_index = 0, even_index = 0;
+        int i=0;
+        for(i=0; i<arr.length; i++)
+        {
+            if(arr[i]%2 == 0)
+            {
+                eve.add(arr[i]);
+                even_index++;
+            }
+            else
+            {
+                odd.add(arr[i]);
+                odd_index++;
+            }
+        }
+        System.out.print("ODD NUMBERS ");
+        display(odd);
+        System.out.print("EVEN NUMBERS: ");
+        display(eve);
+
     }
 
 }
