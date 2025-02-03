a507f3528258fff7df88f95b5e860a91bd2f9c5d
Neil-Cardoz <149564425+Neil-Cardoz@users.noreply.github.com>
2025-02-03 21:17:35 +0530

    Created arrayInput method
    Added the logic for entering the array

--- UserInput.java
+++ UserInput.java
@@ -3,6 +3,16 @@
 import java.util.*;
 
 public class UserInput {
+    public int[] arrayInput() {
+        Scanner scan = new Scanner(System.in);
+        int[] array = new int[5];
+        System.out.println("Enter 5 numbers:");
 
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scan.nextInt();
+        }
+
+        return array;
+    }
 
 }
